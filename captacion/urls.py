84b077cf9140c4b1c73e4660c6ea55fa917eadc2
Roberto Soliz <roberto.soliz@datia.co>
2024-07-21 16:07:47 -0400

    add filters

--- captacion/urls.py
+++ captacion/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
   #  path("", views.index, name="index"),
+  path('contact/<str:name>/', views.contact, name='contact'),
+  path('tipos/', views.tipos, name='tipos'),
   path('index/', views.index),
 ]
--- captacion/views.py
+++ captacion/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Tipo
 
 def index(request):
     return HttpResponse("Hola Roberto")
+
+def contact(request, name):
+    return HttpResponse(f"Bienvendo {name} a la clase de Django")
+
+def tipos(request):
+    filtro_nombre = request.GET.get("nombre")
+    if filtro_nombre:
+       items = Tipo.objects.filter(nombre__contains=filtro_nombre)
+    else:
+        items = Tipo.objects.all()
+    
+    return render(request, "tipos.html", {"tipos": items})
