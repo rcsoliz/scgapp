a12540d3161e5209120bce2b22eea1a1bdcf687f
Roberto Soliz <roberto.soliz@datia.co>
2024-07-21 12:09:57 -0400

    create model and migration

--- captacion/admin.py
+++ captacion/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from .models import Tipo, Ganado, Ganadero, Zona, Estancia
 # Register your models here.
+class TipoAdmin(admin.ModelAdmin):
+    search_fields = ('nombre',)
+
+class GanadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','tipo', 'descripcion', 'disponible',)
+    list_filter = ('disponible', 'tipo',)
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+
+class Ganaderodmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido', 'ci', 'telefono','celular','email', 'disponible')
+    list_filter = ('disponible', 'nombre',)
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+
+class ZonaAdmin(admin.ModelAdmin):
+    search_fields = ('nombre',)
+
+class Estanciadmin(admin.ModelAdmin):
+    list_display = ('nombre','zona', 'descripcion', 'coordenadax','coordenaday', 'disponible')
+    list_filter = ('disponible', 'zona',)
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+
+admin.site.register(Tipo, TipoAdmin)
+admin.site.register(Ganado, GanadoAdmin)
+admin.site.register(Ganadero, Ganaderodmin)
+admin.site.register(Zona, ZonaAdmin)
+admin.site.register(Estancia,Estanciadmin)
--- captacion/models.py
+++ captacion/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=30, unique=True)
+    def __str__(self):
+        return self.nombre
+    
+class Residencia(models.TextChoices):
+    LOCAL = "dpto", 'LOCAL'
+    NACIONAL= 'naci', 'NACIONAL'
+    EXTRAJERO= 'exte', 'EXTRAJERO'
+
+class Ganado(models.Model):
+    nombre= models.CharField(max_length=30, unique=True)
+    tipo =models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=255, blank=True)
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+    disponible = models.BooleanField(blank=True, default=True)
+    Residencia = models.CharField(
+        max_length=4,
+        choices=Residencia.choices,
+        default=Residencia.LOCAL
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.nombre
+
+class Ganadero(models.Model):
+    nombre= models.CharField(max_length=60)
+    apellido= models.CharField(max_length=100)
+    ci= models.CharField(max_length=20)
+    telefono= models.CharField(max_length=20)
+    celular= models.CharField(max_length=12)
+    email= models.CharField(max_length=30)
+    disponible = models.BooleanField(blank=True, default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+
+class Zona(models.Model):
+    nombre= models.CharField(max_length=60)
+    descripcion = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
+class Estancia(models.Model):
+    nombre= models.CharField(max_length=60)
+    zona= models.ForeignKey(Zona, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=200, blank=True)
+    coordenadax= models.CharField(max_length=30)
+    coordenaday= models.CharField(max_length=30)
+    disponible = models.BooleanField(blank=True, default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nombre
+     
